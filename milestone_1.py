a1e8f3df1ac81712f1ec97dc041d9728b11ee36f
Amy Mallett <amymallett8@gmail.com>
2022-09-25 16:55:11 +0100

    "New file added"

---
+++ milestone_1.py
@@ -0,0 +1,17 @@
+# %%
+import random
+
+# %%
+word_list = ["apple", "banana", "orange", "strawberry", "watermelon"]
+
+word = random.choice(word_list)
+
+print(word)
+
+# %%
+guess = input("Choose a letter")
+
+if len(guess)==1:
+    print("Good guess!")
+else: 
+    print("Oops! That is not a valid input")
