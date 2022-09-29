dd09fb0263f8b3baceb79bbea06481b2858431c5
Amy Mallett <amymallett8@gmail.com>
2022-09-29 18:23:23 +0100

    milestone_3.py added

--- milestone_3.py
+++ milestone_3.py
@@ -12,31 +12,38 @@ class Hangman:
         self.word_list = word_list
         self.list_of_guesses = []
 
-
-    def check_guess(guess):
+    def check_guess(self, guess):
         
         guess = guess.lower()
 
-        if guess in word:
+        if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-        else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
+            
+            for index, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[index] = letter
+            self.num_letters -= 1
 
-    def ask_for_input():
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
+        
+        self.list_of_guesses.append(guess)
+
+    def ask_for_input(self):
 
         while True:
             guess = input("Choose a letter")
 
             if guess.isaplha() == False or guess != 1:
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses()
-        
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
 
-
-
-
-
-
-
+ask_for_input()
 
 # %%
