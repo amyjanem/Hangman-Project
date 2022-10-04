92eac3c487e7071dd2e1623cdfd064750a1ce471
Amy Mallett <amymallett8@gmail.com>
2022-10-04 16:47:20 +0100

    milestone_3.py added

--- milestone_3.py
+++ milestone_3.py
@@ -2,6 +2,7 @@
 
 import random
 
+
 class Hangman:
 
     def __init__(self, word_list, num_lives = 5):
@@ -14,30 +15,37 @@ class Hangman:
 
     def check_guess(self, guess):
         guess = guess.lower()
+        
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             print(f"You have {self.num_lives} lives left.")
+            
             if self.word.count(guess) > 1:
                 self.num_letters = self.num_letters - self.word.count(guess)
             else:
                 self.num_letters -= 1
+            
             for letter in range(0, len(self.word)):
                 if self.word[letter] == guess:
                     self.word_guessed[letter] = guess
                     print(self.word_guessed)
                     print(f"You have {self.num_letters} letters left.\n")
+            
             if self.num_letters == 1:
                 print("You're almost there!\n")
+        
         else:
             self.num_lives -= 1
             incorrect_response = ["Oh no!", "Sorry!", "Oh shucks!", "Incorrect!"]
             print(random.choice(incorrect_response), f"{guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left.\n")
+        
         self.list_of_guesses.append(guess)
 
     def ask_for_input(self):
         while True:
             guess = input("Choose a letter")
+            
             if guess.isalpha() == False or len(guess) != 1:
                 print("Invalid letter. Please, enter a single alphabetical character.\n")
             elif guess in self.list_of_guesses:
@@ -46,6 +54,7 @@ class Hangman:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
             break
+
 
 def play_game():
     word_list = ["pear", "orange", "strawberry", "apple", "banana"]
